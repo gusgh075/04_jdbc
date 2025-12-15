33386bb6609ec6d8f7a7a7b1e5897688e2ed2b1e
SungWon180 <qew032032@gmail.com>
2025-12-16 01:45:41 +0900

    12월15일 수업내용-JDBC Class

---
+++ 08_YoonSungWon/chap02-statement-lecture-source/src/main/java/com/qew032/section01/statement/Application2.java
@@ -0,0 +1,66 @@
+package com.qew032.section01.statement;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import static com.qew032.common.JDBCTemplate.close;
+import static com.qew032.common.JDBCTemplate.getConnection;
+
+public class Application2 {
+    public static void main(String[] args) {
+
+        // DB 연결 객체
+        Connection con = null;
+        // SQL 전달, 수행 결과 받아오는 객체
+        Statement stmt = null;
+        // SELECT 수행 결과를 얻어와 저장하는 객체
+        ResultSet rset = null;
+
+        try {
+
+            con = getConnection();
+
+            /* Statement : 쿼리를 운반하고 결과를 반환하는 객체 */
+            stmt = con.createStatement();
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("조회할 메뉴 코드를 입력하세요 : ");
+            int menuCode = sc.nextInt();
+
+            String sql = "select * from tbl_menu where menu_code = " + menuCode;
+
+            // executeQuery() : SELECT 수행 후 ResultSet 반환
+            /* ResultSet : SELECT 결과를 다루는 객체(1행 씩) */
+            rset = stmt.executeQuery(sql);
+
+            if(rset.next()) {    // 다음 행이 있다면 true 반환
+
+                // 한 행에서 특정 컬럼 값을 자바 자료형에 맞춰 얻어오기
+                System.out.println("menu_code : " + rset.getInt("menu_code"));
+                System.out.println("menu_name : " + rset.getString("menu_name"));
+                System.out.println("menu_price : " + rset.getInt("menu_price"));
+                System.out.println("category_code : " + rset.getInt("category_code"));
+                System.out.println("orderable_status : " + rset.getString("orderable_status"));
+                System.out.println("----------------------------------------------------------------------------");
+
+            } else {
+                System.out.println("해당 메뉴 코드와 일치하는 메뉴가 없습니다.");
+            }
+
+
+        } catch(SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+
+            // 프로그램 안정성을 위해 JDBC 객체를 생성 역순으로 반환
+            close(rset);
+            close(stmt);
+            close(con);
+
+        }
+
+    }
+}
