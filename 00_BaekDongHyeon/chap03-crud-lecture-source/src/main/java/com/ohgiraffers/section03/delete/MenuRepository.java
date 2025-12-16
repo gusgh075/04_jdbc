4df03f506e74da22df04a0b1c17a4edc79d35041
donghyeon-100 <goat.ohgiraffers@gmail.com>
2025-12-16 18:16:53 +0900

    과제 : 메뉴 삭제 답안

--- 00_BaekDongHyeon/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section03/delete/MenuRepository.java
+++ 00_BaekDongHyeon/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section03/delete/MenuRepository.java
@@ -1,4 +1,37 @@
 package com.ohgiraffers.section03.delete;
 
+import com.ohgiraffers.section02.update.Menu;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Properties;
+
+import static com.ohgiraffers.common.JDBCTemplate.close;
+
 public class MenuRepository {
+
+  public int deleteMenu(Connection con, int menuCode){
+    int result = 0; // 업데이트 결과 1 또는 0 저장할 변수
+    PreparedStatement pstmt = null;
+    Properties prop = new Properties(); // XML 읽어오기
+
+    try{
+      prop.loadFromXML(new FileInputStream("src/main/java/com/ohgiraffers/mapper/MenuMapper.xml"));
+
+      String sql = prop.getProperty("deleteMenu");
+
+      pstmt = con.prepareStatement(sql);
+
+      pstmt.setInt(1, menuCode);
+
+      result = pstmt.executeUpdate(); // DELETE 수행
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    } finally {
+      close(pstmt);
+    }
+    return result;
+  }
 }
--- 00_BaekDongHyeon/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section03/delete/MenuService.java
+++ 00_BaekDongHyeon/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section03/delete/MenuService.java
@@ -1,4 +1,23 @@
 package com.ohgiraffers.section03.delete;
 
+
+import java.sql.Connection;
+
+import static com.ohgiraffers.common.JDBCTemplate.*;
+
 public class MenuService {
+
+  public int removeMenu(int menuCode){
+    Connection con = getConnection();
+
+    MenuRepository menuRepository = new MenuRepository();
+
+    // repository 메서드를 호출하여 결과(1:성공, 0:실패) 반환 받기
+    int result = menuRepository.deleteMenu(con, menuCode);
+
+    if(result > 0) commit(con);
+    else           rollback(con);
+
+    return result;
+  }
 }
